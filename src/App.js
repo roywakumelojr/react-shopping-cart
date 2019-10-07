5f95664ef2856ece48b8d703830033e7639c9fe5
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-07 12:37:57 -0400

    STEP 3 - Providing data with ProductContext Complete

--- src/App.js
+++ src/App.js
@@ -18,24 +18,17 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Navigation cart={cart} />
+	
+				{/* Routes */}
+				<Route exact path="/" component={Products} />
+	
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
